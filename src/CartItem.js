c714d19721661463f599ae4dc9554ddb74f940b1
revati-tagare <revatitagare2017@gmail.com>
2023-12-31 18:09:41 +0530

    initial commit

---
+++ src/CartItem.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: "Phone",
+      qty: 1,
+      img: "",
+    };
+    //this.increaseQuantity=this.increaseQuantity.bind(this);
+  }
+  increaseQuantity = () => {
+    console.log("this", this.state);
+  };
+  render() {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#77" }}>{price}</div>
+          <div style={{ color: "#77" }}>{qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons*/}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+              onClick={this.increaseQuantity}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/11468/11468231.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+};
+export default CartItem;
